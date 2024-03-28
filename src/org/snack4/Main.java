b918ff6fd1c04f0d2bef78f7f3127f752f284b6d
Alematri <alessandromatricardi@gmail.com>
2024-03-28 16:40:58 +0100

    snacks 4 done

--- src/org/snack4/Main.java
+++ src/org/snack4/Main.java
@@ -1,17 +1,42 @@
 package org.snack4;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Input della stringa dall'utente
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci una testo: ");
+        String input = scanner.nextLine();
+        scanner.close();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Trimmo, tolgo punteggiatura e trasformo in minuscolo
+        String cleanedInput = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        // Verifico se la stringa è palindroma
+        boolean isPalindroma = isPalindroma(cleanedInput);
+
+        // Stampo il risultato
+        if (isPalindroma) {
+            System.out.println(input + " è un testo palindromo.");
+        } else {
+            System.out.println(input + " non è un testo palindromo.");
         }
+    }
+
+    // Metodo isPalindroma
+    public static boolean isPalindroma(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        // Controllo che i caratteri all'estremità sinistra e destra sono uguali
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false; // non è palindroma
+            }
+            start++;
+            end--;
+        }
+        return true; // altrimenti è palindroma
     }
 }
